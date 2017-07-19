188726f78c01c81cd804840722b8c5084b34d554
Yudong <823767532@qq.com>
2017-07-19 14:39:25 +0800

    update

--- spider/Servicedemo/qiuweiService.py
+++ spider/Servicedemo/qiuweiService.py
@@ -1,6 +1,6 @@
 import time
-from entityItem import QuweiItem as quwei
-from Dao import DBControl as DB
+from spider.entityItem import QuweiItem as quwei
+from spider.Dao import DBControl as DB
 
 #去掉重复元素
 def sublist(lists1 , lists2):
